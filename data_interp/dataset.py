337af9faf94a3db6db0f87c389372bd5aa2dc094
undertherainysky <elgl9746@colorado.edu>
2026-04-17 17:00:25 +0000

    added nameguard to dataset

--- data_interp/dataset.py
+++ data_interp/dataset.py
@@ -1,4 +1,5 @@
 # Implement csv, netcdf, xarray, pandas conversions
+ 
 
 import numpy as np
 import pandas as pd
@@ -12,3 +13,11 @@ output_file_path = "out_file.txt"
 def text_input_filepath(in_file_name, out_file_name):
     input_file_path = file_name
     output_file_path = out_file_name
+
+
+
+
+if __name__ == "__main__":
+    from pathlib import Path
+    INfile_path = Path("..").joinpath("data_interpolator").joinpath("sample_data_camulator.nc")
+    OUTfile_path = Path("..").joinpath("data_interpolator").joinpath("test_output.nc")
