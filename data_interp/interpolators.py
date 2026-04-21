23fa1ea046d1f50c77bce690e4f719cdeb1bb462
Ignacio Naus <igna9555@colorado.edu>
2026-04-21 10:27:03 -0600

    fixed inputs for test

--- data_interp/interpolators.py
+++ data_interp/interpolators.py
@@ -14,15 +14,18 @@ class NearestNeighbour():
         self.ds = ds
         self.gr = gr
         try:
-            self.lat = gr[1]
-            self.lon = gr[2]
+            self.lat = gr[0]
+            self.lon = gr[1]
         except IndexError:
             text = 'wrong grid resolution input, please input a tuple with lat & lon'
             return print(text)
 
     def Interpolate(self):
         try:
-            nds = self.ds.sel(self.lat,self.lon,method='nearest')
+            if self.ds == pd.DataFrame():
+                nds = self.ds.interpolate(method='nearest')
+            else:    
+                nds = self.ds.sel(self.lat,self.lon,method='nearest')
             return nds
         except TypeError:
             text = 'wrong dataset input, please input a pd.DataFrame or xr.DataArray'
@@ -35,8 +38,8 @@ class Bilinear():
     def __init__(self,ds,gr):
         self.ds = ds
         try:
-            self.lat = gr[1]
-            self.lon = gr[2]
+            self.lat = gr[0]
+            self.lon = gr[1]
         except IndexError:
             text = 'wrong grid resolution input, please input a tuple with lat & lon'
             return print(text)
@@ -61,6 +64,7 @@ if __name__ == "__main__":
 
     lats_coarse = np.arange(26, 50, 2.0)
     lons_coarse = np.arange(-119, -70, 2.0)
+    new_grid = [lats_coarse, lons_coarse]
 
     LON, LAT = np.meshgrid(lons_fine, lats_fine)
     T = 300 - 0.5 * (LAT - 25) + 3 * np.sin(np.radians(LON + 100))
@@ -71,7 +75,7 @@ if __name__ == "__main__":
         coords={'lat': lats_fine, 'lon': lons_fine}
     )
 
-    ds_nn = NearestNeighbour(ds_fine, lats_coarse,lons_coarse)
+    ds_nn = NearestNeighbour(ds_fine, new_grid)
     print(ds_nn)
-    ds_bl = Bilinear(ds_fine, lats_coarse,lons_coarse)
+    ds_bl = Bilinear(ds_fine, new_grid)
     print(ds_bl)
