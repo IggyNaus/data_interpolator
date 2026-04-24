971b9520be5b6ac96b73d748b9fe947172d36ed7
undertherainysky <elgl9746@colorado.edu>
2026-04-24 04:03:40 +0000

    changed formatting to be optional

--- data_interp/dataset.py
+++ data_interp/dataset.py
@@ -17,6 +17,20 @@ def df_get_col_names(df):
     names = df.columns.tolist()
     return names
 
+def format_df_datetime(df):
+    timeName = input("Please enter name of time column: ")
+    dateName = input("Please enter name of date column: ")
+    time = (
+        df[timeName].astype(str)
+        .str.strip()
+        .str.replace(r"a$", "AM", regex=True)
+        .str.replace(r"p$", "PM", regex=True)
+    )
+    date = pd.to_datetime(df[dateName].astype(str).str.strip() + " " + t,format="%m/%d/%y %I:%M%p",errors="coerce",
+    )
+    df = df.set_index(dt).drop(columns=[dateName, timeName])
+    df.index.name = "datetime"
+
 def dataset_read_csv(file_name):
     """
     Imports csvs as Pandas DFs
@@ -27,18 +41,25 @@ def dataset_read_csv(file_name):
     df = pd.read_csv(read_file_name, header=[0, 1]) # adjust this as necessary for specific CSVs
 
     names = df_get_col_names(df)
-    print(f"Imported DF Columns: {names}") 
+    print(f"Imported DF with Columns: {names}") 
     
-    # Instead of assuming naming conventiosn or trying to search for times and dates directly, have user specify 
-    timeName = input("Please enter name of time column:")
-    dateName = input("Please enter name of date column:")
-    time = (
-        df[time_col].astype(str).str.strip().str.replace(r"a$", "AM", regex=True).str.replace(r"p$", "PM", regex=True)
-    )
-    date = pd.to_datetime(df[date_col].astype(str).str.strip() + " " + t,format="%m/%d/%y %I:%M%p",errors="coerce",
-    )
-    df = df.set_index(dt).drop(columns=[date_col, time_col])
-    df.index.name = "datetime"
+    # Instead of assuming naming conventiosn or trying to search for times and dates directly, have user specify
+    print(f"Try to coerce date and time format?")
+    formatted = False
+    while not(formatted):
+        format = input("[y / n]: ")
+        if (format == "y") | (format == "Y"): 
+            format_df_datetime(df)
+        elif (format == "n") | (format == "N"):
+            break
+        else: 
+            print(f"Invalid input, please try again.")
+            continue
+
+    return df
+    
+
+    
 
 
 def text_input_filepath(in_file_name, out_file_name):
@@ -50,6 +71,7 @@ def text_input_filepath(in_file_name, out_file_name):
 
 if __name__ == "__main__":
     from pathlib import Path
-    INfile_path = Path("..").joinpath("filtered-copy-fixed-crop.csv")
+    INfile_path = Path("..").joinpath("data_interpolator").joinpath("filtered-copy-fixed-crop.csv")
     OUTfile_path = Path("..").joinpath("data_interpolator").joinpath("test_output.nc")
-    dataset_read_csv(INfile_path)
+    df = dataset_read_csv(INfile_path)
+#ATOC_4815/data_interpolator/filtered-copy-fixed-crop.csv
